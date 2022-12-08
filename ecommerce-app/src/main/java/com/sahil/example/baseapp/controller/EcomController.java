401c3d78b9f4210665614ff2ff5ac88816689b3f
sahilsingla <sahil.singla@99acres.com>
2022-12-08 23:15:51 +0530

    ecommerce v2

--- ecommerce-app/src/main/java/com/sahil/example/baseapp/controller/EcomController.java
+++ ecommerce-app/src/main/java/com/sahil/example/baseapp/controller/EcomController.java
@@ -7,7 +7,6 @@ import com.sahil.example.baseapp.model.OrderDto;
 import com.sahil.example.baseapp.model.OrderRespDto;
 import com.sahil.example.baseapp.model.Product;
 import com.sahil.example.baseapp.repository.ProductRepository;
-import com.sun.jndi.toolkit.url.Uri;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +14,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/ecom")
--- order-service/src/main/java/com/sahil/example/baseapp/controller/OrderController.java
+++ order-service/src/main/java/com/sahil/example/baseapp/controller/OrderController.java
@@ -1,12 +1,9 @@
 package com.sahil.example.baseapp.controller;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sahil.example.baseapp.model.OrderDto;
 import com.sahil.example.baseapp.model.Orderinfo;
 import com.sahil.example.baseapp.repository.OrderinfoRepository;
-import com.sun.org.apache.xpath.internal.operations.Or;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
